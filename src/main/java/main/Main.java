50ca74189cf0701de6ea9badb9a2a4ef40f24be3
Pablo <pablo.rico.19@gmail.com>
2022-01-25 18:53:06 +0100

    Test 2

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,7 +7,10 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        
     }
 
+    public void metodo1(){
+        System.out.println("Hello");
+    }
 }
