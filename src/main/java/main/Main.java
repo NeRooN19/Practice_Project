994971bb7610f3c3ec5593600abb0fb358d579b1
Pablo <pablo.rico.19@gmail.com>
2022-01-25 19:04:30 +0100

    Window

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import window.MainFrame;
+
 /**
  *
  * @author NeRooN
@@ -7,6 +9,6 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        
+        new MainFrame();
     }
 }
